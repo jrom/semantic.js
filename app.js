eb21b2cd3c07bff010fe42d1de116348805e9fd0
Jordi Romero <jordi@jrom.net>
2012-05-19 17:19:50 +0200

    less

--- app.js
+++ app.js
@@ -57,11 +57,12 @@ app.configure(function () {
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express['static'](__dirname + '/public'));
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less']}));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(everyauth.middleware());
   app.use(app.router);
+  app.use(express['static'](__dirname + '/public'));
 });
 
 app.configure('development', function () {
