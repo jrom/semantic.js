843388bf5b783990e3c97e0a1304ae57f3c2bd5f
Jordi Romero <jordi@jrom.net>
2012-05-28 18:17:40 +0200

    validate seed, fix seed

--- seed.js
+++ seed.js
@@ -3,12 +3,14 @@ var mongo = require('mongodb')
   , Db = mongo.Db;
 
 var server = new Server('localhost', 27017, {auto_reconnect: true})
-  , db = new Db('semantic', server);
+  , db = new Db('semantic', server)
+  , Item = require('./models/item.js')(db);
 
 var posts = [
   { type: "episode",
     title: "First semantic episode",
     permalink: "first-semantic-episode",
+    excerpt: "Lorem ipsum",
     body: "Lorem ipsum",
     number: 1,
     created_at: new Date("2012-04-20")
@@ -16,6 +18,7 @@ var posts = [
 , { type: "episode",
     title: "Second semantic episode",
     permalink: "second-semantic-episode",
+    excerpt: "Something else",
     body: "Something else",
     number: 2,
     created_at: new Date("2012-04-27")
@@ -23,12 +26,14 @@ var posts = [
 , { type: "post",
     title: "Semantic relaunch",
     permalink: "semantic-relaunch",
+    excerpt: "This is a post explaining the relaunch",
     body: "This is a post explaining the relaunch",
     created_at: new Date("2012-04-19")
   }
 , { type: "link",
     title: "Camaloon offer",
     permalink: "camaloon-offer",
+    excerpt: "Let's explain something about this link",
     body: "Let's explain something about this link",
     url: "http://camaloon.com/some/landing/page",
     created_at: new Date("2012-04-24")
@@ -36,9 +41,16 @@ var posts = [
 ];
 
 db.open(function (err, db) {
-  db.collection('items', function (err, collection) {
-    collection.remove();
-    collection.insert(posts);
+  Item.remove();
+  posts.forEach(function (post) {
+    Item.validateAndInsert(post, function (err, validator) {
+      if (validator.hasErrors()) {
+        console.log('OMG ERRORS', validator.errors);
+      }
+      else {
+        console.log('created ', post.title);
+      }
+    });
   });
   db.close();
 });
