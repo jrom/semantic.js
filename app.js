d7f23f19d46655d7f7e13f465e082810301d22d9
Jordi Romero <jordi@jrom.net>
2012-06-03 21:16:50 +0200

    real pagination value

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ function index(req, res, resource, page) {
     , limit, skip, total_pages;
 
   page = page || 0;
-  limit = 1;
+  limit = 20;
   skip = page * limit;
 
   if (resource === 'podcast') {
